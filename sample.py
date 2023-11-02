9cedfd9bd5027aede766fddadcd60b23df88bb92
Nalla9239 <akarshreddy97@gmail.com>
2023-11-02 17:58:29 -0400

    added weighing converter code

---
+++ sample.py
@@ -0,0 +1,140 @@
+# ls = [9,5,7,3]
+# ls.append(1)
+# print(ls)
+
+# sum = 1
+# for a in ls:
+#     sum = a * sum
+#     print(f"Ans = {sum}")
+# if sum == 1000:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# ls = [9,5,7,3]
+# ls.insert(0,1)
+# print(ls)
+
+# sum = 1
+# for a in ls:
+#     sum = a * sum
+#     print(f"Ans = {sum}")
+# if sum == 1000:
+#     print("YES")
+# else:
+#     print("NO")
+
+# print("@" * 10)
+
+
+# name = input("what is your name  ")
+# colour = input("fav colour  ")
+# print(name +   " likes "   + colour)
+
+
+# birth_year = input("what is your birth year?  ")
+# age = 2023 - int(birth_year)
+# print(age)
+
+# weight_pounds = input("What is your weight in pounds? ")
+# weight = 0.45359237 * float(weight_pounds)
+# print(weight)
+
+# output = """hi john,
+# how are you"""
+# print(output)
+
+# 
+
+
+# x = 10
+# # x = x + 3
+# x += 3
+# print(x)
+
+# x = (2 + 3) * 10 - 3
+# print(x)
+
+# x = 2.9
+# print(round(x))
+# print(abs(-2.9))
+
+# import math
+
+
+# credit_score = 900
+# if credit_score <= 700:
+#     print("put down 10%")
+# else:
+#     print("put down 20%")
+
+# good_credit = True
+# if good_credit:
+#     print("put down 10%")
+# else:
+#     print("put down 20%")
+
+
+# good_credit = True
+# price = 1000000
+# if good_credit:
+#     down_payment = 0.1 * price
+    
+# else:
+#     down_payment = 0.2 * price
+# print(f"Down payment = ${down_payment}")
+
+# good_credit = True
+# good_salary = True
+
+# if good_credit and not good_salary:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+# temperature = 20
+# if temperature > 30:
+#     print("It's a hot day")
+# elif temperature < 10:
+#     print("It's a cold day")
+# else:
+#     print("It's hot nor cold")
+
+
+# name = "Akarsh alalalalalalalalalalalalalalallalalalalalalalalallalalalallallalallalallalala"
+# if len(name) < 3:
+#     print("Name must be atleast 3 characters")
+# elif len(name) > 50:
+#     print("Name can be maximum of 50 characters")
+# else:
+#     print("Name looks good")
+
+
+weight = input("weight ")
+converter = input("(L)bs or (K)g: ")
+Lbs = float(weight) * 2.20
+Kg = float(weight) * 0.45
+if converter == "L":
+    print(Lbs)
+else:
+    print(Kg)
+
+
+# weight = input("weight ")
+# converter = input("Lbs or Kg: ")
+# Lbs = float(weight) * 2.20
+# Kg = float(weight) * 0.45
+# if converter == "Lbs":
+#     print(Lbs)
+# else:
+#     print(Kg)
+
+
+
+
+
+
+
+   
+
